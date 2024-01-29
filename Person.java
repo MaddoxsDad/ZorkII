66513fd5484a3b8f3dd3bf355c7ad92a59df01b7
Kevin Landry <klandry@cpsc.us-central1-a.c.cpsc-department-server.internal>
2024-01-28 21:17:15 -0500

    can't get to compile

--- Person.java
+++ Person.java
@@ -1,20 +1,41 @@
-public class Person
-   private String firstName;
-   private String lastName;
-   private String mmName; //Mothers maiden name
-   private String birthCity;
+   public class Person{
+    private String firstName;
+    private String lastName;
+    private String mmName; //Mothers maiden name
+    private String birthCity;
 
-   public Person(String firstName, String lastName, String mmName, String birthCity) {
-       this.firstName = firstName;
-       this.lastName = lastName;
-       this.mmName = mmName;
-       this.birthCity = birthCity;
- } 
-//Getters and Setters
-   public String getFirstName() {
-       return firstName;
- }
-   public void setFirstName(String firstName) {
-       this.firstName = firstName;
-   }
+    public Person(String firstName, String lastName, String mmName, String     birthCity) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mmName = mmName;
+        this.birthCity = birthCity;
+    }
+  //Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+ 
+    public String getMmName() {
+        return mmName;
+    }
+
+    public String getBirthCity() {
+        return birthCity;
+    }
+ 
+    public void setBirthCity(String birthCity) {
+        this.birthCity = birthCity;
+    }
+  
